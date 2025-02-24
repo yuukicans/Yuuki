32a890b7d5cd62aca2a106feec29d1920c5f3c08
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-24 16:42:52 +0700

    c shell.py
    Signed-off-by: petinggint <155607304+petinggint@users.noreply.github.com>

--- FallenRobot/modules/shell.py
+++ FallenRobot/modules/shell.py
@@ -46,7 +46,7 @@ SHELL_HANDLER = CommandHandler(["sh"], shell, run_async=True)
 
 dispatcher.add_handler(SHELL_HANDLER)
 
-__mod_name__ = "Shell"
+__mod_name__ = "sʜᴇʟʟ"
 
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]
