26ce03b66fa00af794096d8cfdc45b28104013bd
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-24 15:49:06 +0700

    c antiflood.py
    Signed-off-by: petinggint <155607304+petinggint@users.noreply.github.com>

--- FallenRobot/modules/antiflood.py
+++ FallenRobot/modules/antiflood.py
@@ -412,7 +412,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Aɴᴛɪ-Fʟᴏᴏᴅ"
+__mod_name__ = "ᴀɴᴛɪ-ғʟᴏᴏᴅ"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.chat_type.groups,
