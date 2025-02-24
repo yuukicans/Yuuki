8ce45f3fac2f496581480f2e1152acc36ad28614
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-24 16:35:54 +0700

    c warns.py
    Signed-off-by: petinggint <155607304+petinggint@users.noreply.github.com>

--- FallenRobot/modules/warns.py
+++ FallenRobot/modules/warns.py
@@ -517,7 +517,7 @@ be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is
  ❍ `/strongwarn <on/yes/off/no>`*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
 """
 
-__mod_name__ = "Wᴀʀɴs"
+__mod_name__ = "ᴡᴀʀɴs"
 
 WARN_HANDLER = CommandHandler(
     ["warn", "dwarn"], warn_user, filters=Filters.chat_type.groups, run_async=True
