13e1633b04b1b3b802811ce975bc5af5966a4626
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-24 16:22:05 +0700

    c dev.py
    Signed-off-by: petinggint <155607304+petinggint@users.noreply.github.com>

--- FallenRobot/modules/dev.py
+++ FallenRobot/modules/dev.py
@@ -88,6 +88,6 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "Dᴇᴠ"
+__mod_name__ = "ᴅᴇᴠ"
 
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
