e1428da429f1343f9a7093928d18e8c0454c3fb7
petinggint <155607304+petinggint@users.noreply.github.com>
2025-02-24 16:10:12 +0700

    c blacklistusers.py
    Signed-off-by: petinggint <155607304+petinggint@users.noreply.github.com>

--- FallenRobot/modules/blacklistusers.py
+++ FallenRobot/modules/blacklistusers.py
@@ -154,5 +154,5 @@ dispatcher.add_handler(BL_HANDLER)
 dispatcher.add_handler(UNBL_HANDLER)
 dispatcher.add_handler(BLUSERS_HANDLER)
 
-__mod_name__ = "Blacklisting Users"
+__mod_name__ = "ʙʟᴀᴄᴋʟɪsᴛɪɴɢ ᴜsᴇʀs"
 __handlers__ = [BL_HANDLER, UNBL_HANDLER, BLUSERS_HANDLER]
