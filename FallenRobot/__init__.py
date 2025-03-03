6c86b116855f34c1cd8ad3b008f0874f6aea2532
petinggint <155607304+petinggint@users.noreply.github.com>
2025-03-03 20:47:45 +0700

    c __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -49,7 +49,7 @@ if ENV:
         "START_IMG", "https://files.catbox.moe/pp9026.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "infomusiccalvin")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "InfoRobotCalvin")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
