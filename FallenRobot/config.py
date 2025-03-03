3e567784199c5948fab326f97686ec59ececec07
petinggint <155607304+petinggint@users.noreply.github.com>
2025-03-03 20:44:06 +0700

    c config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -16,7 +16,7 @@ class Config(object):
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://files.catbox.moe/pp9026.jpg"
 
-    SUPPORT_CHAT = "infomusiccalvin"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "InfoRobotCalvin"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
