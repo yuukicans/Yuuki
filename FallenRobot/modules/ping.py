52a1307eb9987e656ca02134335eea8eec1160ce
petinggint <155607304+petinggint@users.noreply.github.com>
2025-03-06 16:46:24 +0700

    c ping.py

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -45,9 +45,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ᴍᴜsɪᴄ ᴍᴀɴᴀɢᴇ ᴄᴀʟᴠɪɴ ɪᴅᴜᴘ ʙᴀʙʏ! 🔥\n"
-        "<b>ᴛɪᴍᴇ:</b> <code>{}</code>\n"
-        "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "ᴄλʟᴠɪɴ ꭙ ꝛᴏʙᴏᴛ ᴀᴄᴛɪᴠᴇ🔥\n━━━━━━━━━━━━━\n"
+        "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>\n"
+        "<b>ᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
