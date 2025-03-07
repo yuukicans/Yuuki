52690722a4130044c18227afd91cd9c68b99d249
yuukicans <calvinmanagement15@gmail.com>
2025-03-07 17:13:03 +0700

    yuuki ping.py

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -45,7 +45,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ᴄλʟᴠɪɴ ꭙ ꝛᴏʙᴏᴛ ᴀᴄᴛɪᴠᴇ🔥\n━━━━━━━━━━━━━\n"
+        "Yuuki Music ᴀᴄᴛɪᴠᴇ🔥\n━━━━━━━━━━━━━\n"
         "<b>ᴜᴘᴛɪᴍᴇ :</b> <code>{}</code>\n━━━━━━━━━━━━━\n"
         "<b>ᴀᴋɪᴛᴘ ᴅᴀʀɪ :</b> <code>{}</code>━━━━━━━━━━━━━\n".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
