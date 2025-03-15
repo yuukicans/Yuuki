330bb29de0005fcb535f57f5aa9f75105b65aa1c
yuukicans <calvinmanagement15@gmail.com>
2025-03-15 12:42:00 +0700

    yuuki __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -46,7 +46,7 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://files.catbox.moe/0qe835.jpg"
+        "START_IMG", "https://files.catbox.moe/1u9rd3.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "inforobotcalvin")
